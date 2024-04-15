e72cda9a2f4d8e794e2600c60b531db29a437a22
Tihara Egodage <tiharaeg@gmail.com>
2024-04-15 17:38:55 +0530

    user crud functions

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -21,13 +21,86 @@ const createNewUser = asyncHandler(async(req,res) => {
 
     //check for duplicate
     const duplicate = await User.findOne({username}).lean().exec()
+
+    if (duplicate) {
+        return res.status(409).json({message: 'Duplicate username'})
+    }
+
+    //hash password
+    const hashedPwd = await bcrypt.hash(password, 10) //salt rounds
+
+    const userObject = {username, "password" : hashedPwd, roles}
+
+    //create and store the new user
+    const user = await User.create(userObject)
+
+    if(user) {
+        res.status(201).json({message: `New user ${username} created`})
+    } else {
+        res.status(400).json({message: 'Invalid user data recieved'})
+    }
 })
 
+
 const updateUser = asyncHandler(async(req,res) => {
+
+    const {id, username, roles, active, password} = req.body
+
+    //confirm data
+    if (!id || !username || !Array.isArray(roles) || !roles.length || typeof active !== 'boolean') {
+        return res.status(400).json({message: 'All fields are required'})
+    }
+
+    const user = await User.findById(id).exec()
+
+    if (!user) {
+        return res.status(404).json({message: 'User not found'})
+    }
+
+    const duplicate = await User.findOne({username}).lean().exec()
+    //allow updates to the original user
+    if (duplicate && duplicate?._id.toString() !== id) {
+        return res.status(409).json({message: 'Duplicate username'})
+    }
+
+    user.username = username
+    user.roles = roles
+    user.active = active
+
+    if (password) {
+        user.password = await bcrypt.hash(password, 10)
+    }
+
+    const updatedUser = await user.save()
+
+    res.json({message: `${updatedUser.username} updated`})
 
 })
 
 const deleteUser = asyncHandler(async(req,res) => {
+    const {id} = req.body
+
+    if (!id) {
+        return res.status(400).json({message: 'User ID is required'})
+    }
+
+    const notes = await Note.findOne({user: id}).lean().exec()
+
+    if (notes?.length) {
+        return res.status(400).json({message : 'User has assigned notes'})
+    }
+
+    const user = await User.findById(id).exec()
+
+    if(!user) {
+        return res.status(404).json({message: 'User not found'})
+    }
+
+    const result = await user.deleteOne()
+
+    const reply = `Username ${result.username} with ID ${result._id} deleted`
+
+    res.json(reply)
 
 })
 
