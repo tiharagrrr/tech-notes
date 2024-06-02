912e5dc05b4249c86502a8a5c5394c33e6185314
Tihara Egodage <tiharaeg@gmail.com>
2024-06-02 20:59:51 +0530

    authcontroller

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,10 +3,57 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const asyncHandler = require('../middleware/asyncHandler');
 
-// @desc  Login
+//@desc  Login
 //@route POST /auth
 //@access Public
 const login = asyncHandler(async (req, res) => {
+    const {username, password} = req.body;
+
+    if (!username || !password) {
+        return res.status(400).json({message: 'All fields are required'})
+    }
+
+    const foundUser = await User.findOne({username}).exec()
+
+    if (!foundUser || !foundUser.active) {
+        return res.status(401).json({message: 'Unauthorized'})
+    }
+
+    const match = await bcrypt.compare(password, foundUser.password)
+
+    if (!match) {
+        return res.status(401).json({message: 'Unauthorized'})
+    }
+
+    const accessToken = jwt.sign(
+        {
+            "UserInfo" : {
+                "username": foundUser.username,
+                "roles": foundUser.roles
+            }
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: '10s' //just while in development
+        }
+    )
+
+    const refreshToken = jwt.sign(
+        { "username" : foundUser.username },
+        process.env.REFRESH_TOKEN_SECRET,
+        {   expiresIn: '1d' }
+    )
+
+    //create cookie with refresh token
+    res.cookie('jwt', refreshToken, {
+        httpOnly: true, //cookie cannot be accessed by client side scripts, accessible only by web server (not thru javascript)
+        secure: true ,//cookie can only be sent over https
+        sameSite: 'None', //cookie can be sent to any site
+        maxAge: 86400000 //1 day , set to match rT
+    })
+
+    //send accessToken containing username and roles
+    res.json({accessToken})
 
 })
 
@@ -14,6 +61,38 @@ const login = asyncHandler(async (req, res) => {
 //@route GET /auth/refresh
 //@access Public - because access token has expired
 const refresh = (req,res) => {
+    const cookies = req.cookies
+
+    if (!cookies?.jwt) return res.status(401).json({message: 'Unauthorized'})
+
+    const refreshToken = cookies.jwt
+
+    jwt.verify(
+        refreshToken,
+        process.env.REFRESH_TOKEN_SECRET,
+        asyncHandler(async (err, decoded) => {
+            if (err) return res.status(403).json({message: 'Forbidden'})
+
+            const foundUser = await User.findOne({username: decoded.username}).exec()
+
+            if (!foundUser) return res.status(401).json({message: 'Unauthorized'})
+
+            const accessToken = jwt.sign(
+                {
+                    "UserInfo" : {
+                        "username": foundUser.username,
+                        "roles": foundUser.roles
+                    }
+                },
+                process.env.ACCESS_TOKEN_SECRET,
+                {
+                    expiresIn: '10s' //just while in development
+                }
+            )
+
+            res.json({accessToken})
+        })
+    )
 
 }
 
@@ -21,6 +100,14 @@ const refresh = (req,res) => {
 //@route POST /auth/logout
 //@access Public - just to clear cookie if exists
 const logout = (req,res) => {
+    const cookies = req.cookies
+    if (!cookies?.jwt) return res.sendStatus(204) //no content
+    res.clearCookie('jwt', {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'None'
+    })
+    res.json({message: 'Cookie cleared'})
 
 }
 
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,12 +4,12 @@ const authController = require('../controllers/authController')
 const loginLimiter = require('../middleware/loginLimiter')
 
 router.route('/')
-    .post(loginLimiter, )
+    .post(loginLimiter, authController.login )
 
 router.route('/refresh')
-    .get()
+    .get(authController.refresh)
 
 router.route('/login')
-    .post()
+    .post(authController.logout)
 
 module.exports = router
